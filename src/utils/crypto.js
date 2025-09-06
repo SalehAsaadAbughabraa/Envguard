96084df07226c517e15f90608a31712b641ca895
Saleh abughabra <saleh87alally@gmail.com>
2025-09-06 15:06:24 +0300

    Update crypto.js

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,1 +1,17 @@
+const crypto = require('crypto');
 
+const ALGORITHM = 'aes-256-gcm';
+const KEY_LENGTH = 32;
+const IV_LENGTH = 12;
+
+function encrypt(plaintext, password) {
+  const iv = crypto.randomBytes(IV_LENGTH);
+  const key = crypto.scryptSync(password, iv.toString('hex'), KEY_LENGTH);
+  const cipher = crypto.createCipheriv(ALGORITHM, key, iv);
+  let encrypted = cipher.update(plaintext, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  const authTag = cipher.getAuthTag();
+  return iv.toString('hex') + encrypted + authTag.toString('hex');
+}
+
+module.exports = { encrypt };
